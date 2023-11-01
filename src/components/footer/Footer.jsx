39d68a2101cc2a143a01f6c26360fbd86a38f61e
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-11-01 15:22:55 +0300

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./Footer.scss";
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <footer>
+      <div className="left-side">
+        <div className="footer-logo">
+          <a href="/">Hoca Metre</a>
+        </div>
+        <div className="links">
+          <a href="/">Ana Sayfa</a> | <a href="/hakkinda">Hakkımızda</a>
+        </div>
+      </div>
+      <div>
+        <h4>İletişime Geç</h4>
+        <form>
+          <input type="email" placeholder="E-posta adresi" />
+          <textarea placeholder="Mesajınızı buraya yazın" rows="4"></textarea>
+          <button type="submit">Gönder</button>
+        </form>
+      </div>
+    </footer>
+  );
+};
 
-export default footer
+export default Footer;
